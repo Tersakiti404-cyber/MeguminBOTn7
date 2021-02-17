93e7fd0e73e8fe140a27fe7627faf00a49a75c24
Tersakiti404-cyber <76648497+Tersakiti404-cyber@users.noreply.github.com>
2021-02-17 20:18:10 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 apt-get update
 apt-get upgrade
-apt-get install nodejs
-apt-get install libwebp
-apt-get install ffmpeg
-apt-get install wget
-apt-get install tesseract
+apt-get install nodejs -y
+apt-get install libwebp -y
+apt-get install ffmpeg -y
+apt-get install wget -y
+apt-get install tesseract -y
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm i imgbb-uploader
 npm cache clean -f
